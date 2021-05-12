e81db535a4a65dbbe25cfeda8ec8c9fc245e17f6
Paulo Victor <paulovictorlima418@gmail.com>
2021-05-11 21:03:25 -0300

    AdCalculator

--- calculator.js
+++ calculator.js
@@ -13,20 +13,6 @@ O mesmo anúncio é compartilhado no máximo 4 vezes em sequência
   - Os novos compartilhamentos geram 72% de novas views
 
 Crie um script em sua linguagem de programação preferida que receba o valor investido em reais e retorne uma projeção aproximada da quantidade máxima de pessoas que visualizarão o mesmo anúncio (considerando o anúncio original + os compartilhamentos)
-
-const investiment = 250
-const views = investiment * 30
-const views2 = views * 0.72
-const views3 = views2 * 0.72
-const views4 = views3 * 0.72
-const totalViews = views + views2 + views3 + views4
-
-console.log(`O valor investido foi: ${investiment}R$`);
-console.log(`Projeção da quantidade de views no anúncio original é: ${views} Views`);
-console.log(`Projeção aproximada da quantidade de pessoas que visualizarão o anúncio compartilhado pela PRIMEIRA pessoa é: ${views2.toFixed(2)} Views `);
-console.log(`Projeção aproximada da quantidade de pessoas que visualizarão o anúncio compartilhado pela SEGUNDA pessoa é: ${views3.toFixed(2)} Views`);
-console.log(`Projeção aproximada da quantidade de pessoas que visualizarão o anúncio compartilhado pela TERCEIRA pessoa é: ${views4.toFixed(2)} Views`);
-console.log(`Projeção da quantidade TOTAL de views com os compartilhamentos é: ${totalViews.toFixed(2)} Views`);
 */
 
 function calculator() {
